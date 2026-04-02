ce08ee1662afd0d734d29ac791deacf06946d6b6
Vinícius <viniciuscalegari123b5@gmail.com>
2026-04-01 22:18:20 -0300

    Correção

--- scripit.js
+++ scripit.js
@@ -1,13 +1,22 @@
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", addTask);
+
 function addTask() {
-    const input = document.getElementById("taskInput");
-    const list = document.getElementById("taskList");
+    if (input.value.trim() === "") {
+        alert("Digite uma tarefa!");
+        return;
+    }
 
     const li = document.createElement("li");
     li.textContent = input.value;
 
-    li.onclick = function() {
+    // Marcar como concluída (Stage 2)
+    li.addEventListener("click", function () {
         li.style.textDecoration = "line-through";
-    };
+    });
 
     list.appendChild(li);
     input.value = "";
